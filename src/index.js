598f700a03d374bea7623e51b7a3089dfb4f7dd2
Bakhrom <baxromsadullayev@mail.ru>
2019-07-06 19:17:01 +0500

    Finished Task

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
+module.exports = function getLoveTrianglesCount(preferences = []) {
+    let result = 0;
+
+    preferences.forEach((element, i) => {
+
+        let secondElem = preferences[element - 1]
+        let thirdElem = preferences[secondElem - 1]
+         if (
+             preferences[preferences[element - 1] - 1] === i + 1 &&
+             element !== secondElem
+            ) {
+            result++
+        }
+    })
+    result /= 3;
+    return result;
+}
